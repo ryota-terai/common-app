fffdd7cd5d46c8f53c2714c8a7a9cd3b125731c2
ryota-terai <96863681+ryota-terai@users.noreply.github.com>
2022-04-16 14:37:18 +0900

    update docker

--- docker/payara/setup_payara.sh
+++ docker/payara/setup_payara.sh
@@ -2,5 +2,5 @@
 
 asadmin --user admin --passwordfile /opt/payara/passwordFile add-resources glassfish-resources.xml
 asadmin --user admin --passwordfile /opt/payara/passwordFile add-resources glassfish-resources2.xml
-asadmin --user admin --passwordfile /opt/payara/passwordFile deploy GISApp.ear
-asadmin --user admin --passwordfile /opt/payara/passwordFile deploy COMMONApp.ear
+#asadmin --user admin --passwordfile /opt/payara/passwordFile deploy GISApp.ear
+#asadmin --user admin --passwordfile /opt/payara/passwordFile deploy COMMONApp.ear
